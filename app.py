5b8becf4b5ee3da06ed2f4f463b685adad5f48d7
mwin <moekyawwin@gmail.com>
2024-04-04 00:13:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 st.title('Hello World!')
-st.write('This is a simple Streamlit app hosted on Heroku.')
+st.write('This is a simple Streamlit app hosted on Herok.')
+st.write('I will use .....')
